71eedc51f72ce01bea30f4622431e5da48a1e64a
Giorgos Gkogkou <ggkogkou@ece.auth.gr>
2023-01-05 15:47:01 +0200

    Removed Unnecessary Variables

--- linear_analysis.m
+++ linear_analysis.m
@@ -1,28 +1,12 @@
-function linear_analysis(timeseries, N)
+function linear_analysis(timeseries, N, id)
     % Add analysis tools to the path
     addpath("lab/");
 
-    % Load timeseries datafiles
-    addpath("EruptionData/");
-    geysers_1989 = load("EruptionData/eruption1989.dat");
-    geysers_2000 = load("EruptionData/eruption2000.dat");
-    geysers_2011 = load("EruptionData/eruption2011.dat");
-
-    length_1989 = length(geysers_1989);
-    length_2000 = length(geysers_2000);
-    length_2011 = length(geysers_2011);
-
-    % random_point = floor((length_2000 - length_1989) * rand());
-    % random_point = floor((length_2011 - length_1989) * rand());
-
-    geysers_2000 = geysers_2011(278:575);
-    geysers_2011 = geysers_2011(893:1190);
-
-    % Linear Analysis for 1989
+    % Linear Analysis 
     figure(1);
-    r_t = autocorrelation(geysers_1989, length_1989, 'ac.f. 1989', 'c');
+    r_t = autocorrelation(timeseries, N, id, 'c');
     hold on;
-    confidence_interval = [-1.96/sqrt(length_1989) 1.96/sqrt(length_1989)];
+    confidence_interval = [-1.96/sqrt(N) 1.96/sqrt(N)];
     yline(confidence_interval(1), 'r');
     yline(confidence_interval(2), 'r');
 
@@ -32,7 +16,7 @@ function linear_analysis(timeseries, N)
             p_value = p_value + 1;
         end
     end
-    p_value = p_value / length_1989;
+    p_value = p_value / N;
     if p_value > 0.05
         fprintf("There are strong correlations between observations, it's not white noise. %.3f is significant\n", p_value);
     else
@@ -43,7 +27,7 @@ function linear_analysis(timeseries, N)
     aic_memory_ar = dictionary(1, 0);
 
     for p=1 : 30
-        [~, ~, ~, ~, aic_memory_ar(p)] = fitARMA(geysers_1989, p, 0, 1);
+        [~, ~, ~, ~, aic_memory_ar(p)] = fitARMA(timeseries, p, 0, 1);
     end
 
     aic_array = values(aic_memory_ar);
@@ -54,7 +38,7 @@ function linear_analysis(timeseries, N)
     aic_memory_ma = dictionary(1, 0);
 
     for q=1 : 30
-        [~, ~, ~, ~, aic_memory_ma(q)] = fitARMA(geysers_1989, 0, q, 1);
+        [~, ~, ~, ~, aic_memory_ma(q)] = fitARMA(timeseries, 0, q, 1);
     end
 
     aic_array = values(aic_memory_ma);
@@ -67,7 +51,7 @@ function linear_analysis(timeseries, N)
     figure(4);
     for q=1 : 7
         for p=1 : 7
-            [~, ~, ~, ~, aic_memory_arma(p)] = fitARMA(geysers_1989, p, q, 1);
+            [~, ~, ~, ~, aic_memory_arma(p)] = fitARMA(timeseries, p, q, 1);
         end
         plot(values(aic_memory_arma));
         q_str{q} = sprintf('q = %d', q);
@@ -76,12 +60,12 @@ function linear_analysis(timeseries, N)
         drawnow;
     end
 
-    % Find models parameters
-    [nrmse, phi_paramaeters, theta_parameters, s_z, aic, ~, ~] = fitARMA(geysers_1989, 1, 1, 1)
+    % Find models parameters for selected p, q
+    [nrmse, phi_paramaeters, theta_parameters, s_z, aic, ~, ~] = fitARMA(timeseries, 1, 1, 1)
 
-    [nrmse, phi_paramaeters, ~, s_z, aic, ~, ~] = fitARMA(geysers_1989, 2, 0, 1)
+    [nrmse, phi_paramaeters, ~, s_z, aic, ~, ~] = fitARMA(timeseries, 2, 0, 1)
 
-    [nrmse, ~, theta_parameters, s_z, aic, ~, ~] = fitARMA(geysers_1989, 0, 5, 1)
+    [nrmse, ~, theta_parameters, s_z, aic, ~, ~] = fitARMA(timeseries, 0, 5, 1)
 
 end
 
--- main.m
+++ main.m
@@ -8,11 +8,15 @@ function main()
     geysers_2000 = load("EruptionData/eruption2000.dat");
     geysers_2011 = load("EruptionData/eruption2011.dat");
 
-    length_1989 = length(geysers_1989);
-    length_2000 = length(geysers_2000);
-    length_2011 = length(geysers_2011);
+    % random_point = floor((length_2000 - length_1989) * rand());
+    % random_point = floor((length_2011 - length_1989) * rand());
 
-    linear_analysis(geysers_1989, length_1989);
+    length_1989 = length(geysers_1989);
+
+    geysers_2000 = geysers_2011(278:575);
+    geysers_2011 = geysers_2011(893:1190);
+
+    linear_analysis(geysers_1989, length_1989, '1989');
 
 
 end
